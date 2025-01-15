2dbd21ba4c2eabf43fa09b8e93e4b91e1005aa95
zeroo | retardhook.xyz <153208404+zerootoad@users.noreply.github.com>
2025-01-15 20:17:52 +0100

    More instructions and rewrote the decoding switch

--- src/i8080/instructions.zig
+++ src/i8080/instructions.zig
@@ -1,15 +1,6 @@
 const std = @import("std");
 const CPU = @import("cpu.zig").CPU;
 
-fn set_cy(cpu: *CPU, val: u16) void {
-    cpu.flags.cy = (val & 0xFF00) != 0;
-}
-
-fn set_ac(cpu: *CPU, a: u8, b: u8, carry: bool) void {
-    const sum = (a & 0x0F) + (b & 0x0F) + (if (carry) 1 else 0);
-    cpu.flags.ac = (sum & 0x10) != 0;
-}
-
 fn set_p(cpu: *CPU, val: u8) void {
     var x: u8 = 0;
     for (0..8) |i| {
@@ -37,6 +28,20 @@ pub fn decode(cpu: *CPU) Instruction {
     std.debug.print("Decoding opcode: {x}\n", .{opcode});
 
     switch (opcode) {
+        0x00, 0x10, 0x20, 0x30, 0x08, 0x18, 0x28, 0x38 => return Instruction{ .execute = NOP, .size = 1, .mnemonic = "NOP" },
+        0x01, 0x11, 0x21, 0x31 => return Instruction{ .execute = LXI, .size = 3, .mnemonic = "LXI RP, d16" },
+        0x02, 0x12 => return Instruction{ .execute = STAX, .size = 1, .mnemonic = "STAX (RP)" },
+        0x22 => return Instruction{ .execute = SHLD, .size = 3, .mnemonic = "SHLD a16" },
+        0x32 => return Instruction{ .execute = STA, .size = 1, .mnemonic = "STA a16" },
+        0x03, 0x13, 0x23, 0x33 => return Instruction{ .execute = INX, .size = 3, .mnemonic = "INX RP" },
+        0x04, 0x14, 0x24, 0x34 => return Instruction{ .execute = INR, .size = 1, .mnemonic = "INR RM" },
+        0x05, 0x15, 0x25, 0x35 => return Instruction{ .execute = DCR, .size = 1, .mnemonic = "DCR RM" },
+        0x06, 0x16, 0x26, 0x36, 0x0E, 0x1E, 0x2E, 0x3E => return Instruction{ .execute = MVI, .size = 2, .mnemonic = "MVI RM, d8" },
+        0x07 => return Instruction{ .execute = RLC, .size = 1, .mnemonic = "RLC" },
+        0x17 => return Instruction{ .execute = RAL, .size = 1, .mnemonic = "RAL" },
+        0x37 => return Instruction{ .execute = STC, .size = 1, .mnemonic = "STC" },
+        0x0F => return Instruction{ .execute = RRC, .size = 1, .mnemonic = "RRC" },
+        0x1F => return Instruction{ .execute = RAR, .size = 1, .mnemonic = "RAR" },
         else => unreachable,
     }
 }
@@ -84,8 +89,8 @@ fn STAX(cpu: *CPU) void {
 }
 
 fn SHLD(cpu: *CPU) void {
-    const low_byte: u8 = @as(u8, @intCast(cpu.mem[cpu.regs.pc + 1]));
-    const high_byte: u8 = @as(u8, @intCast(cpu.mem[cpu.regs.pc + 2])) << 8;
+    const low_byte: u8 = cpu.mem[cpu.regs.pc + 1];
+    const high_byte: u8 = cpu.mem[cpu.regs.pc + 2];
 
     cpu.regs.l = low_byte;
     cpu.regs.h = high_byte;
@@ -96,7 +101,7 @@ fn SHLD(cpu: *CPU) void {
 
 fn MVI(cpu: *CPU) void {
     const opcode = cpu.mem[cpu.regs.pc];
-    const low_byte: u8 = @as(u8, @intCast(cpu.mem[cpu.regs.pc + 1]));
+    const low_byte: u8 = cpu.mem[cpu.regs.pc + 1];
 
     switch (opcode) {
         0x06 => {
@@ -131,6 +136,50 @@ fn MVI(cpu: *CPU) void {
     cpu.regs.pc += 2;
 }
 
+fn RAL(cpu: *CPU) void {
+    const msb = cpu.regs.a & 0x80;
+    const carry: u8 = if (cpu.flags.cy) 1 else 0;
+
+    cpu.regs.a = cpu.regs.a << 1 | carry;
+
+    if (msb != 0) cpu.flags.cy = true else cpu.flags.cy = false;
+    cpu.regs.pc += 1;
+}
+
+fn RAR(cpu: *CPU) void {
+    const lsb = cpu.regs.a & 0x01;
+    const carry: u8 = if (cpu.flags.cy) 0x80 else 0;
+
+    cpu.regs.a = cpu.regs.a >> 1 | carry;
+
+    if (lsb != 0) cpu.flags.cy = true else cpu.flags.cy = false;
+
+    cpu.regs.pc += 1;
+}
+
+fn RLC(cpu: *CPU) void {
+    const msb = cpu.regs.a & 0x80; // most significant byte
+
+    cpu.regs.a = cpu.regs.a << 1 | msb >> 7;
+
+    if (msb != 0) cpu.flags.cy = true else cpu.flags.cy = false;
+    cpu.regs.pc += 1;
+}
+
+fn RRC(cpu: *CPU) void {
+    const lsb = cpu.regs.a & 0x01; // least significant byte
+
+    cpu.regs.a = cpu.regs.a >> 1 | lsb << 7;
+
+    if (lsb != 0) cpu.flags.cy = true else cpu.flags.cy = false;
+    cpu.regs.pc += 1;
+}
+
+fn STC(cpu: *CPU) void {
+    cpu.flags.cy = true;
+    cpu.regs.pc += 1;
+}
+
 fn STA(cpu: *CPU) void {
     const addr: u16 = @intCast(@as(u16, @intCast(cpu.mem[cpu.regs.pc + 1])) | @as(u16, @intCast(cpu.mem[cpu.regs.pc + 2])) << 8);
 
@@ -156,7 +205,7 @@ fn INR(cpu: *CPU) void {
     const opcode = cpu.mem[cpu.regs.pc];
 
     switch (opcode) {
-        0x05 => {
+        0x04 => {
             const b = cpu.regs.b + 1;
             cpu.regs.b = b;
             cpu.set_bc(cpu.get_bc());
@@ -170,7 +219,7 @@ fn INR(cpu: *CPU) void {
 
             set_zsp(cpu, c);
         },
-        0x15 => {
+        0x14 => {
             const d = cpu.regs.d + 1;
             cpu.regs.d = d;
             cpu.set_de(cpu.get_de());
@@ -184,7 +233,7 @@ fn INR(cpu: *CPU) void {
 
             set_zsp(cpu, e);
         },
-        0x25 => {
+        0x24 => {
             const h = cpu.regs.h + 1;
             cpu.regs.h = h;
             cpu.set_hl(cpu.get_hl());
@@ -198,7 +247,7 @@ fn INR(cpu: *CPU) void {
 
             set_zsp(cpu, l);
         },
-        0x35 => {
+        0x34 => {
             const hl = cpu.get_hl();
             const valhl = cpu.mem[hl] + 1;
             cpu.mem[hl] = valhl;
@@ -228,7 +277,7 @@ fn DCR(cpu: *CPU) void {
 
             set_zsp(cpu, b);
         },
-        0x0C => {
+        0x0D => {
             const c = cpu.regs.c - 1;
             cpu.regs.c = c;
             cpu.set_bc(cpu.get_bc());
@@ -242,7 +291,7 @@ fn DCR(cpu: *CPU) void {
 
             set_zsp(cpu, d);
         },
-        0x1C => {
+        0x1D => {
             const e = cpu.regs.e - 1;
             cpu.regs.e = e;
             cpu.set_de(cpu.get_de());
@@ -256,7 +305,7 @@ fn DCR(cpu: *CPU) void {
 
             set_zsp(cpu, h);
         },
-        0x2C => {
+        0x2D => {
             const l = cpu.regs.l - 1;
             cpu.regs.l = l;
             cpu.set_hl(cpu.get_hl());
@@ -270,7 +319,7 @@ fn DCR(cpu: *CPU) void {
 
             set_zsp(cpu, valhl);
         },
-        0x3C => {
+        0x3D => {
             const a = cpu.regs.a - 1;
             cpu.regs.a = a;
 
--- src/main.zig
+++ src/main.zig
@@ -7,19 +7,35 @@ pub fn main() !void {
 
     cpu.mem[0] = 0x01;
     cpu.mem[1] = 0x34;
-    cpu.mem[2] = 0x12; // LXI B, 0x1234
-    cpu.mem[3] = 0x02; // STAX B
-    cpu.mem[4] = 0x04; // INR B
-    cpu.mem[5] = 0x14; // INR D
-    cpu.mem[6] = 0x24; // INR H
-    cpu.mem[7] = 0x34; // INR (HL)
+    cpu.mem[2] = 0x12;
+    cpu.mem[3] = 0x02;
+    cpu.mem[4] = 0x04;
+    cpu.mem[5] = 0x14;
+    cpu.mem[6] = 0x24;
+    cpu.mem[7] = 0x34;
+    cpu.mem[8] = 0x07;
+    cpu.mem[9] = 0x17;
+    cpu.mem[10] = 0x37;
+    cpu.mem[11] = 0x0F;
+    cpu.mem[12] = 0x1F;
 
     //try cpu.load("src/roms/8080EXM.COM");
-    std.debug.print("Initial program counter: PC = {x}\n", .{cpu.regs.pc});
+    std.debug.print("Initial PC: PC = {X}\n\n", .{cpu.regs.pc});
 
-    for (0..6) |_| {
+    for (0..13) |_| {
         cpu.cycle();
-        std.debug.print("After execution: PC = {x}\n", .{cpu.regs.pc});
-        std.debug.print("BC register pair: {x}\n", .{cpu.get_bc()});
+        cpu_state(&cpu);
     }
+}
+
+fn cpu_state(cpu: *icpu.CPU) void {
+    std.debug.print("Registers: PC = {}, SP = {}, A = {X}, BC = {X}, DE = {X}, HL = {X}\n", .{ cpu.regs.pc, cpu.regs.sp, cpu.regs.a, cpu.get_bc(), cpu.get_de(), cpu.get_hl() });
+
+    std.debug.print("Flags: Z = {}, S = {}, P = {}, AC = {}, CY = {}\n", .{ cpu.flags.z, cpu.flags.s, cpu.flags.p, cpu.flags.ac, cpu.flags.cy });
+
+    std.debug.print("Memory Dump: ", .{});
+    for (0..16) |i| {
+        std.debug.print("{X} ", .{cpu.mem[i]});
+    }
+    std.debug.print("\n\n", .{});
 }
